feff8fef2e3b1097554e266e3a42dd97b9d725c5
shariqazeem <shariqshaukat786@gmail.com>
2024-04-26 16:14:11 +0500

    added email host password with env

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -37,7 +37,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'shariqshaukat786@gmail.com'
-EMAIL_HOST_PASSWORD = 'fkaednmfukelytgu'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 # Application definition
 
